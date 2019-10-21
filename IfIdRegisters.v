2d150ccb536aa5da9f7323efb11eecad32941f9f
Pang Siyuan <eypidanh@163.com>
2019-10-21 19:00:06 +0800

    -------------remove useless-----------------

---
+++ IfIdRegisters.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+
+module IfIdRegisters (
+		input clk,
+		input rst,
+		input id_shouldStall,
+		input [31:0] if_pc_4,
+		input [31:0] if_instruction,
+		output reg [31:0] id_pc_4 = 0,
+		output reg [31:0] id_instruction = 0
+	);
+
+	always @(posedge clk) begin
+		if (rst) begin
+			id_pc_4 <= 0;
+			id_instruction <= 0;
+		end else if (id_shouldStall) begin
+			id_pc_4 <= id_pc_4;
+			id_instruction <= id_instruction;
+		end else begin
+			id_pc_4 <= if_pc_4;
+			id_instruction <= if_instruction;
+		end
+	end
+endmodule
---
+++ StallControlLogic.v
@@ -0,0 +1,9 @@
+`timescale 1ns / 1ps
+module StallControlLogic(
+    input [31:0]inst_in,
+    input [31:0]Data_in,
+
+    output id_shouldStall
+    );
+    
+endmodule
